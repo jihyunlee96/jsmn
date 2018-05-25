3c3d388d41c468cac6a6b2a5b118af3a0bacb9f7
jihyunlee96 <21700581@handong.edu>
2018-05-25 11:24:02 +0900

    modified parsejsonfile.c

--- example/parsejsonfile.c
+++ example/parsejsonfile.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "jsmn.h"
+#include "../jsmn.h"
+#include "../jsmn.c"
 
 /*
  * A small example of jsmn parsing when JSON structure is known and number of
